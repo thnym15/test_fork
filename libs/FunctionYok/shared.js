e63bfc22828e0b28ecd71bb6b6175b490ed39f70
Sorawee-P <Sorawee.p@dcs.premier.co.th>
2020-10-01 16:26:00 +0700

    add sub func.

--- libs/FunctionYok/shared.js
+++ libs/FunctionYok/shared.js
@@ -1,5 +1,9 @@
 // test share service
 // add new service by air
-function sumNumber(x,y){
-    return x+y;
+function sumNumber(x, y) {
+  return x + y;
+}
+
+function subNumber(x, y) {
+  return x - y;
 }
